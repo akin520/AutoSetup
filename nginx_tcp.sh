c6f381665771daa033b6f7e3e1d00c17cd81eff4
akin520 <h3ewhack@163.com>
2019-10-23 13:19:47 +0800

    fix zlib

--- nginx_tcp.sh
+++ nginx_tcp.sh
@@ -10,9 +10,11 @@ cd ..
 
 wget http://nginx.org/download/nginx-1.17.4.tar.gz
 wget --no-check-certificate -O openssl-1.1.1c.tar.gz https://www.openssl.org/source/openssl-1.1.1c.tar.gz
+wget http://prdownloads.sourceforge.net/libpng/zlib-1.2.11.tar.gz
 
 tar xf nginx-1.17.4.tar.gz
 tar xf openssl-1.1.1c.tar.gz
+tar xf zlib-1.2.11.tar.gz
 
 cd  nginx-1.17.4
 ./configure --prefix=/usr/local/nginx-tcp --user=www --group=www --with-stream --with-http_stub_status_module --with-http_v2_module --with-http_ssl_module --with-http_gzip_static_module --with-http_realip_module --with-pcre=../pcre-8.43 --with-pcre-jit --with-zlib=../zlib-1.2.11 --with-openssl=../openssl-1.1.1c
